7c9be78f82b6a3af11caf49564c19068155432cd
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-10-25 22:43:26 +0200

    activated typeORM: @Entity tells to map with database, @Column maps through concerned columns

--- src/planet/entities/planet.entity.ts
+++ src/planet/entities/planet.entity.ts
@@ -1,5 +1,10 @@
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'planet' })
 export class Planet {
+  @Column()
   name: string;
 
+  @Column()
   distanceToEarth: number;
 }
--- src/starship/entities/starship.entity.ts
+++ src/starship/entities/starship.entity.ts
@@ -1,7 +1,13 @@
+import { Column, Entity } from 'typeorm';
+
+@Entity({ name: 'starship' })
 export class Starship {
+  @Column()
   name: string;
 
+  @Column()
   speed: number;
 
+  @Column()
   kilometerPrice: number;
 }
