eedb13e0cdc82ccc81d8138f648cd8f593561fcc
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2022-10-27 09:25:58 +0200

    Modified Planet services; use DTO and repository, use UUID inmethods

--- src/planet/dto/create-planet.dto.ts
+++ src/planet/dto/create-planet.dto.ts
@@ -1,1 +1,20 @@
-export class CreatePlanetDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { IsString, IsNumber, IsBoolean } from 'class-validator';
+
+export class CreatePlanetDto {
+  @ApiProperty()
+  @Expose()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @Expose()
+  @IsNumber()
+  distanceToEarth: number;
+
+  @ApiProperty()
+  @Expose()
+  @IsBoolean()
+  active: boolean;
+}
